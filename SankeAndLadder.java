a723838a9488b724cc9b11275897132c0cb92474
Akash Das <akashd595@gmail.com>
2022-05-21 13:44:44 +0530

    ADDED: rolling the dice

--- SankeAndLadder.java
+++ SankeAndLadder.java
@@ -1,9 +1,14 @@
 package com.bridgelabz.snake_nd_ladder;
 
+import java.util.Random;
+
 public class SankeAndLadder {
     public static void main(String[] args) {
         System.out.println("Welcome to Snake and Ladder Program on Master Branch");
         System.out.println("Inintial position of the player1 is at 0");
 
+        Random random = new Random();
+        int dice= random.nextInt(6)+1;
+        System.out.println("Player is rolling dice and get "+dice);
     }
 }
