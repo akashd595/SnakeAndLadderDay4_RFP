31345b3b9790c6ec4384bf4a7caac8816fefe6b7
Akash Das <akashd595@gmail.com>
2022-05-21 13:31:45 +0530

    ADDED: snake and ladder in the master branch

---
+++ SankeAndLadder.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.snake_nd_ladder;
+
+public class SankeAndLadder {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Snake and Ladder Program on Master Branch");
+
+    }
+}
