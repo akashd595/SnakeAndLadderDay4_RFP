37c13219fd7d6a0ad8bf7304918464e3fb6d7a33
Akash Das <akashd595@gmail.com>
2022-05-21 13:38:22 +0530

    ADDED: initial position of the player1

--- SankeAndLadder.java
+++ SankeAndLadder.java
@@ -3,6 +3,7 @@ package com.bridgelabz.snake_nd_ladder;
 public class SankeAndLadder {
     public static void main(String[] args) {
         System.out.println("Welcome to Snake and Ladder Program on Master Branch");
+        System.out.println("Inintial position of the player1 is at 0");
 
     }
 }
